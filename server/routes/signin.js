ef77b1973e15a7595ecadcb8a7cf8b01880c1883
xolanihealth <xolanihealth@gmail.com>
2023-01-10 13:10:45 +0100

    Deleted All Files

--- server/routes/signin.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const signInController = require("../controllers/signInController");
-
-//create, find, update, delete
-router.get("/", signInController.signin);
-router.post("/", signInController.auth);
-
-module.exports = router;
--- server/routes/signup.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const signUpController = require("../controllers/signUpController");
-
-//create, find, update, delete
-router.get("/signup", signUpController.signup);
-router.post("/signup", signUpController.insert);
-
-module.exports = router;
