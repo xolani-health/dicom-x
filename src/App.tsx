1807afd7d12afbf7a96121e1802e04bb4862b694
Idowu Damilare Muyiwa Gabriel <dammy@damilareidowu.com>
2023-01-27 08:40:45 +0100

    Added SideBar Toggle

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import SideBar from "./components/SideBar/SideBar"
 export default function App() {
   const [isDarkMode, setIsDarkMode] = useState(true)
   return (
-    <div className="text-white h-full border-2 rounded-md shadow-black shadow-md font-roboto">
+    <div className="text-white h-full border-4 rounded-md shadow-black shadow-md font-roboto overflow-hidden">
       <div className="w-full h-full flex">
         <SideBar />
         <div className="flex flex-col w-full h-full">
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -3,10 +3,18 @@ import React, { useState } from "react"
 export default function SideBar() {
   const [shown, setShown] = useState(true)
   return (
-    <aside className="bg-teal-400 w-1/6 p-3 flex flex-col">
+    <aside className="bg-teal-700 w-fit flex flex-col">
       {/* Logo */}
-      <div>
-        <img />
+      <div className="h-14 p-1 flex justify-between items-center mt-1 w-max">
+        <img src={`/assets/images/dicomx-logo.png`} className={`h-full`} />
+        <span
+          className={`material-symbols-outlined ml-2 bg-black/50 p-2 rounded-md cursor-pointer`}>
+          arrow_forward_ios
+        </span>
+      </div>
+
+      <div className="h-full flex justify-center items-center font-roboto font-bold text-2xl overflow-scroll">
+        Collaboration Panel
       </div>
     </aside>
   )
